f880ba291209edc052a99a93dd227b558581970b
Kelwin Esechiel <90807426+kelwinxd@users.noreply.github.com>
2025-11-22 12:09:33 -0300

    configuração do app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,12 @@
 import { Routes } from '@angular/router';
+import { CreateTaskComponent } from './pages/create-task/create.task.component/create.task.component';
+import { EditTaskComponent } from './pages/edit-task/edit.task.component/edit.task.component';
+import { ListTaskComponent } from './pages/list-task/list.task.component/list.task.component';
 
-
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo:"tasks", pathMatch:"full"},
+    {path:"tasks",component: ListTaskComponent},
+    {path:"edit/:id",component: EditTaskComponent},
+    {path:"create",component: CreateTaskComponent},
+    {path:"**",redirectTo:"tasks"} //poderia criar uma component UI 404
+];
