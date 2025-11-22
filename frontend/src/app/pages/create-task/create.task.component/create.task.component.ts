ca138cccc1a372d7b00a4f6273e4ca90db229ab9
Kelwin Esechiel <90807426+kelwinxd@users.noreply.github.com>
2025-11-22 10:46:16 -0300

    criação de create task html

--- frontend/src/app/pages/create-task/create.task.component/create.task.component.ts
+++ frontend/src/app/pages/create-task/create.task.component/create.task.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create.task.component',
+  standalone:true,
   imports: [ReactiveFormsModule],
   templateUrl: './create.task.component.html',
   styleUrl: './create.task.component.css',
