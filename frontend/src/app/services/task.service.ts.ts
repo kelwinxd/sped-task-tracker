9e1aa669288e64d67ca7fc6136ebabf299a73443
Kelwin Esechiel <90807426+kelwinxd@users.noreply.github.com>
2025-11-22 09:44:37 -0300

    finalização dos metodos http dentro do service

--- frontend/src/app/services/task.service.ts.ts
+++ frontend/src/app/services/task.service.ts.ts
@@ -15,4 +15,20 @@ export class TaskServiceTs {
     getTasks(): Observable<TaskModel[]> {
       return this.http.get<TaskModel[]>(this.apiUrl)
     }
+
+    getTask(id : number): Observable<TaskModel> {
+      return this.http.get<TaskModel>(`${this.apiUrl}/${id}`)
+    }
+
+    createTask(body: {titulo:string,descricao:string}) : Observable<TaskModel> {
+      return this.http.post<TaskModel>(this.apiUrl, body)
+    }
+
+    updateTask(id:number, body: Partial<TaskModel>): Observable<TaskModel> {
+      return this.http.put<TaskModel>(`${this.apiUrl}/${id}`, body)
+    }
+
+    deleteTask(id:number) : Observable<void> {
+      return this.http.delete<void>(`${this.apiUrl}/${id}`)
+    }
 }
